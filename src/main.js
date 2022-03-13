27c8fb394d2c90818537d7e5b8b12f800ecce648
BossWangST <bosswang1226@gmail.com>
2022-03-13 17:13:23 +0800

    added layout for profile page.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// babel-plugin-import 会帮助你加载 JS 和 CSS
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.use(Antd).mount('#app');
