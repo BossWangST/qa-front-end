5922772f12b70cfcc8afd59af6ee454566351238
BossWangST <bosswang1226@gmail.com>
2022-03-14 17:55:10 +0800

    added radar chart for scores.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Layout/>
+  <apexchart/>
 </template>
 
 <script>
 import Layout from "@/components/lay-out.vue";
+import apexchart from "@/components/radar-chart"
 
 export default {
   name: 'App',
   components: {
-    Layout
+    Layout,
+    apexchart
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 // babel-plugin-import 会帮助你加载 JS 和 CSS
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import VueApexCharts from "vue3-apexcharts";
 const app = createApp(App);
 
-app.use(Antd).mount('#app');
+app.use(Antd).use(VueApexCharts).mount('#app');
