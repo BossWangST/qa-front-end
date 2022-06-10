dabf30c39422323e7df764388d7579581968e1b8
BossWangST <bosswang1226@gmail.com>
2022-06-10 19:53:32 +0800

    Updated Personal page
    更新了提问模块，同时从个人信息页可以回到首页了。在考虑怎么把折线图加进去，可能但一个饼状图有点少

---
+++ src/components/LineChart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="wrapper">
+    <div id="chart-line">
+      <apexchart type="line" height="160" :options="chartOptions" :series="series"></apexchart>
+    </div>
+    <div id="chart-line2">
+      <apexchart type="line" height="160" :options="chartOptionsLine2" :series="seriesLine2"></apexchart>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+
+export default {
+  name: "LineChart",
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+
+      series: [1, 2, 3],
+      chartOptions: {
+        chart: {
+          id: 'fb',
+          group: 'social',
+          type: 'line',
+          height: 160
+        },
+        colors: ['#008FFB']
+      },
+
+      seriesLine2: [4, 5, 6],
+      chartOptionsLine2: {
+        chart: {
+          id: 'tw',
+          group: 'social',
+          type: 'line',
+          height: 160
+        },
+        colors: ['#546E7A']
+      },
+
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
