49dc658be74760cd0f969829cf82241a91731222
CharlesWXR <19190211@stu.njnu.edu.cn>
2022-06-17 10:47:55 +0800

    question browse page finished(ish?)

--- src/components/SubjectMenu.vue
+++ src/components/SubjectMenu.vue
@@ -1,25 +1,17 @@
 <template>
   <a-row justify="space-around">
-    <a-col
-      v-for="(subject, index) in menuState.subjects"
-      :key="index"
-      :column="2"
-    >
-      <subject-with-badage
-        :count="menuState.counts[index]"
-        :subject="subject"
-        :subject-id="subject"
-        :selected="selectedSubject == subject"
-        @click="onMenuClick"
-      />
+    <a-col v-for="(subject, index) in menuState.subjects" :key="index" :column="2">
+      <subject-with-badage :count="menuState.counts[index]" :subject="subject" :subject-id="subject"
+        :selected="selectedSubject == subject" @click="onMenuClick" />
     </a-col>
   </a-row>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, reactive, getCurrentInstance } from "vue";
 import { sum } from "lodash";
 import SubjectWithBadage from "@/components/SubjectWithBadage.vue";
+import { findIndex } from "lodash";
 
 export default {
   name: "SubjectMenu",
@@ -36,6 +28,9 @@ export default {
     },
   },
   setup() {
+    const { appContext } = getCurrentInstance();
+    const $http = appContext.config.globalProperties.$http;
+
     const selectedSubject = ref("全部");
     const menuState = reactive({
       subjects: [
@@ -50,10 +45,20 @@ export default {
         "政治",
         "地理",
       ],
-      counts: [0, 100, 20, 3, 4, 5, 6, 7, 8, 9],
+      counts: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     });
 
-    menuState.counts[0] = sum(menuState.counts);
+    $http.get("/question/count").then(response => {
+      let res = response.data;
+      if (res.code === 200) {
+        let counts = res.result;
+        counts.forEach(count => {
+          let index = findIndex(menuState.subjects, t => t == count.subjectName);
+          menuState.counts[index] = count.count;
+        });
+        menuState.counts[0] = sum(menuState.counts);
+      }
+    })
 
     return {
       menuState,
