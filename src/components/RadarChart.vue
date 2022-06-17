2bb4ed9988f86794e207f9c015026db7e9f7f852
BossWangST <bosswang1226@gmail.com>
2022-06-17 11:44:01 +0800

    增加了雷达图获取数据

--- src/components/RadarChart.vue
+++ src/components/RadarChart.vue
@@ -1,40 +1,52 @@
 <template>
   <div id="chart">
     <apexchart
-      type="radar"
-      height="350"
-      :options="chartOptions"
-      :series="series"
+        type="radar"
+        height="350"
+        :options="chartOptions"
+        :series="series"
     ></apexchart>
   </div>
 </template>
 
 <script>
 import VueApexCharts from "vue3-apexcharts";
+import {getCurrentInstance, ref, reactive} from "vue";
 
 export default {
   name: "RadarChart",
   components: {
     apexchart: VueApexCharts,
   },
+  setup() {
+    const {appContext} = getCurrentInstance();
+    const $http = appContext.config.globalProperties.$http;
+    const listData = ref([]);
+
+    const user_id = sessionStorage.getItem('user_id');
+    const series = reactive({
+      name: "得分",
+      data: []
+    })
+    const getGrade = (user_id) => $http.get("/MyGrade/" + user_id).then(
+        response => {
+          let res = response.data;
+          if (res.code == 200) {
+            let r = res.result;
+            series.data = r.map(t => t.value);
+            //console.log(r.grade);
+          }
+        }
+    );
+    getGrade(user_id);
+    return {
+      series,
+      listData,
+      getGrade,
+    }
+  },
   data() {
     return {
-      series: [
-        {
-          name: "得分",
-          data: [
-            (110 / 160) * 100,
-            (180 / 200) * 100,
-            (104 / 120) * 100,
-            (110 / 120) * 100,
-            92,
-            98,
-            (105 / 120) * 100,
-            92,
-            90,
-          ],
-        },
-      ],
       chartOptions: {
         chart: {
           animations: {
@@ -61,17 +73,12 @@ export default {
             "语文",
             "数学",
             "英语",
-            "物理",
-            "历史",
-            "生物",
-            "化学",
-            "政治",
-            "地理",
           ],
         },
       },
     };
   },
+  methods: {}
 };
 </script>
 
